6becca3c76d5c9fb4d9af071b51e8a412178e08b
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-03-29 09:06:11 +0800

    longer delay

--- components/Layouts/BlankLayout.tsx
+++ components/Layouts/BlankLayout.tsx
@@ -11,7 +11,7 @@ export const BlankLayout = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 600);
+    setTimeout(() => setIsLoading(false), 700);
   }, []);
 
   return (
