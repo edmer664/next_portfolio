3525c62d11a5e460e5f1564f0e69009840a76a85
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-03-30 18:22:00 +0800

    meta tags SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,48 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initalProps = await Document.getInitialProps(ctx)
+    const initalProps = await Document.getInitialProps(ctx);
 
-    return initalProps
+    return initalProps;
   }
 
   render() {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/assets/images/logo.svg" type="image/x-icon" />
+          <link
+            rel="shortcut icon"
+            href="/assets/images/logo.svg"
+            type="image/x-icon"
+          />
+          <meta name="title" content="Edmerson Pizarra" />
+          <meta
+            name="description"
+            content="Hello, I’m Edmerson Pizarra, a 17-year-old Software Developer, based in the Philippines."
+          />
+          <meta
+            name="keywords"
+            content="Edmer, Edmerson, Pizarra, Web Developer, PocketDevs, Programmer, Freelancer, Filipino, commission, science, bataan, Philippines, Edmerkun, hikari, John, computer, Developer, Software, Solutions, IT, problem, solving"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="author" content="John Edmerson Liggu Pizarra"></meta>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
