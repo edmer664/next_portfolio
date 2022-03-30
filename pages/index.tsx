b1fc9bfb549ff93f5d7884a3c14c95f4502054d8
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-03-30 17:23:14 +0800

    new updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,8 @@ import Image from "next/image";
 import ProjectCards from "../components/ProjectCards";
 import Link from "next/link";
 
-
 const Home: NextPage = () => {
-  const theme = useContext(ThemeContext);
+  
 
   return (
     <BlankLayout>
@@ -37,7 +36,10 @@ const Home: NextPage = () => {
             </Link>
           </div>
         </article>
-        <div className="flex justify-center md:justify-end lg:w-1/3" data-aos="fade-left">
+        <div
+          className="flex justify-center md:justify-end lg:w-1/3"
+          data-aos="fade-left"
+        >
           <Image
             width={400}
             height={550}
@@ -51,11 +53,17 @@ const Home: NextPage = () => {
 
       {/* WHAT I OFFER SECTION */}
       <section className="py-20 px-7 lg:px-40">
-        <h2 className="pb-24 text-4xl font-bold md:pb-32 md:text-5xl" data-aos="fade-right">
+        <h2
+          className="pb-24 text-4xl font-bold md:pb-32 md:text-5xl"
+          data-aos="fade-right"
+        >
           What I offer
         </h2>
         <div className="flex flex-col md:flex-row ">
-          <article className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50" data-aos="fade-up" >
+          <article
+            className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50"
+            data-aos="fade-up"
+          >
             <Image
               width={100}
               height={100}
@@ -66,7 +74,10 @@ const Home: NextPage = () => {
             <h3 className="py-4 font-bold">Elegant Design</h3>
             <p>Receive your website in a clean and elegant UI.</p>
           </article>
-          <article className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50" data-aos="fade-up" >
+          <article
+            className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50"
+            data-aos="fade-up"
+          >
             <Image
               width={100}
               height={100}
@@ -80,7 +91,10 @@ const Home: NextPage = () => {
               quality services.
             </p>
           </article>
-          <article className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50" data-aos="fade-up" >
+          <article
+            className="flex flex-col justify-start px-16 py-10 mx-2 text-center transition-all rounded-lg duration-600 dark:hover:bg-black md:w-1/3 hover:shadow-xl hover:bg-cyan-50"
+            data-aos="fade-up"
+          >
             <Image
               width={100}
               height={100}
@@ -97,52 +111,45 @@ const Home: NextPage = () => {
         </div>
       </section>
       {/* END WHAT I OFFER SECTION */}
+      {/* PROJECTS SECTION */}
       <section className="py-20 px-7 lg:px-40">
-        <h2 className="pb-24 text-4xl font-bold md:pb-32 md:text-5xl" data-aos="fade-up">
+        <h2
+          className="pb-24 text-4xl font-bold md:pb-32 md:text-5xl"
+          data-aos="fade-up"
+        >
           Recent Projects
         </h2>
         <div className="flex flex-col w-full pb-32">
           <ProjectCards
-            imageURL="/assets/images/mockup.jpg"
-            title="Lorem ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing 
-            elit. Donec ac interdum risus, vitae eleifend velit. Etiam
-            nec mi faucibus, accumsan dui eu, consectetur ante.
-            Cras eleifend nibh sapien, eget sagittis sem laoreet et.
-            Nunc suscipit molestie elit, ut dictum ante 
-            pellentesque et Maecenas tempus porttitor dui, et
+            imageURL="/assets/images/projects/confession.png"
+            title="Confession Board"
+            description="An Open-Source website dedicated to letting people express their secrets anonymously. Using Django and PostgreSQL as the backend and NextJs + Bootstrap for the Front-end.
             "
+            url="https://confession.edmer.xyz"
           />
           <ProjectCards
             imageURL="/assets/images/mockup.jpg"
-            title="Lorem ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing 
-            elit. Donec ac interdum risus, vitae eleifend velit. Etiam
-            nec mi faucibus, accumsan dui eu, consectetur ante.
-            Cras eleifend nibh sapien, eget sagittis sem laoreet et.
-            Nunc suscipit molestie elit, ut dictum ante 
-            pellentesque et Maecenas tempus porttitor dui, et
+            title="Pocket Hub"
+            description="A fully-featured social website implemented using PHP laravel and javascript. A fully functional CRUD application, feat includes but is not limited to posts, comments, and messages.
             "
           />
           <ProjectCards
-            imageURL="/assets/images/mockup.jpg"
-            title="Lorem ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing 
-            elit. Donec ac interdum risus, vitae eleifend velit. Etiam
-            nec mi faucibus, accumsan dui eu, consectetur ante.
-            Cras eleifend nibh sapien, eget sagittis sem laoreet et.
-            Nunc suscipit molestie elit, ut dictum ante 
-            pellentesque et Maecenas tempus porttitor dui, et
+            imageURL="/assets/images/projects/wpi2.png"
+            title="WPI Enrollment System"
+            description="An unofficial website for West Philippines Institute implemented using React, JavaScript, Python, and Django. The app uses Django as the backend to perform backend processing for enrollment system and information board.
             "
+            url="https://wpi.edmer.xyz"
           />
-          
         </div>
         <div className="mx-auto text-center">
-        <Link href="/projects">
-          <a className="px-24 py-5 border-2 border-neutral-800 bg-neutral-800 text-neutral-200 hover:border-neutral-700 hover:bg-neutral-700">View All</a>
-        </Link>
+          <Link href="/projects">
+            <a className="px-24 py-5 border-2 border-neutral-800 bg-neutral-800 text-neutral-200 hover:border-neutral-700 hover:bg-neutral-700">
+              View All
+            </a>
+          </Link>
         </div>
       </section>
+      {/* PROJECTS END */}
     </BlankLayout>
   );
 };
